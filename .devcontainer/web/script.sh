01a4682e9486464e5fd2f035a7c6546da6669d83
Alejandro <ale.luis@gmail.com>
2024-09-07 11:10:19 -0300

    zsh by default

--- .devcontainer/web/script.sh
+++ .devcontainer/web/script.sh
@@ -8,4 +8,8 @@ echo >> ~/.bashrc
 echo "alias l=\"ls -lah --color --group-directories-first\"" >> ~/.bashrc
 echo "alias c=\"clear\"" >> ~/.bashrc
 
+echo >> ~/.zshrc
+echo "alias l=\"ls -lah --color --group-directories-first\"" >> ~/.zshrc
+echo "alias c=\"clear\"" >> ~/.zshrc
+
 exit 0
